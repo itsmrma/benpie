fa6ed524a133828e2caed1e5cdae7ec9b4edc3de
itsmrma <71072396+itsmrma@users.noreply.github.com>
2024-04-08 08:34:18 +0200

    ciao pietro

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
 <?php
   echo "Ciao mondo";
   echo "Hello World";
+
+  $url = "https://www.dati.lombardia.it/resource/hs8z-dcey.json";
+  $filename = basename($url);
+  if (file_put_contents($filename, file_get_contents($url))) {
+    echo "File downloaded.";
+  } else {
+    echo "Failed.";
+  }
+
+  if (file_exists($filename)) {
+    $data = file_get_contents($filename);
+    $json = json_decode($data);
+    print_r($json);
+  }
 ?>
