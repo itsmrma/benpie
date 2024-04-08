e919216180dd04b5b8bc1a4811548a12d72d06c6
itsmrma <m.benassi30@gmail.com>
2024-04-08 09:37:13 +0200

    ggg

--- index.php
+++ index.php
@@ -8,29 +8,30 @@
 
   $url = "https://www.dati.lombardia.it/resource/hs8z-dcey.json";
   $filename = basename($url);
-  if (file_put_contents($filename, file_get_contents($url))) {
-    echo "File downloaded.";
-  } else {
-    echo "Failed.";
-  }
+  file_put_contents($filename, file_get_contents($url));
 
   if (file_exists($filename)) {
     $data = file_get_contents($filename);
     $json = json_decode($data);
-    
     echo "<table border=1px>";
     $count=0;
-    foreach($json as $row){
+    foreach ($json as $row) {
       echo "<tr>";
       
-      foreach($row as $key=> $value){
-        if(!$count){
-          echo "<td>".$key."</td>";
-        }else{
-          echo "<td>".$value."</td>";
-        }
-        
+      foreach($row as $key => $value) {
+        echo "<td>" . $key . "</td>";
       }
+
+      echo "</tr> <tr>";
+      foreach($row as $key => $value) {
+        if (gettype($value)=="array") {
+          $json2 = json_decode($value);
+          echo "<td>" . $json2['url'] . "</td>"; 
+        } else {
+          echo "<td>" . $value . "</td>";
+        }
+      }
+      
       $count+=1;
       echo "</tr>";
     }
