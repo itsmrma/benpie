d89f8a0070b2834fa7472dd07935b0bf174fa3ca
itsmrma <m.benassi30@gmail.com>
2024-04-24 09:46:56 +0200

    godo

---
+++ search.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include 'head.html';?>
+</head>
+<body>
+    <?php include 'code.html';?>
+
+    <div class="main-container">
+        
+    </div>
+
+    <script>
+        // set active
+        var active = document.getElementById('sidebar_search');
+        active.classList.add('active');
+    </script>
+</body>
+</html>
