49504488979d16216255fc0b748f44fc881b8e5b
Pietro <savioli.pietro@gmail.com>
2024-04-28 11:15:06 +0200

    avanzamento login con hash

--- login.php
+++ login.php
@@ -13,20 +13,40 @@
     <div class="main-container">
         <form method='post'>
           <br>Mail: <input type='text' name='email'>
-          <?php 
-            if(isset($_POST['mail'])){
+          <br>Password: <input type='text' name='password'>
+          <?php
+            if(isset($_POST['mail'], $_POST['password'])){
               $conn = new mysqli("localhost","root","","sagre");
             
               if ($conn -> connect_error) {
                 die("Errore di connessione ".$conn->connect_errno." ".$conn->connect_error);
               }
-              
-              $sql = "select email from utenti where email=".$_POST["email"];
-              
+
+              $sql = "select email, password from utenti where email=".$_POST["email"];
+
               $result = $conn ->query($sql);
+              $campi=$result->fetch_assoc();
+
+              if(!isset($campi["email"])){
+                echo "<br>Nome: <input type='text' name='nome'>
+                <br>Cognome: <input type='text' name='cognome'>
+                <br>Nome utente: <input type='text' name='userName'>";
+                $password = $_POST['password'];
+                $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+                $sql = "insert into utenti (utenti.nomeUtente, utenti.email, utenti.password, utenti.nome, utenti.cognome)
+                values ()";
+
+              }else if($_POST['email']==$campi["email"] && password_verify($_POST['password'],$campi["password"])){
+                session_start();
+                $_SESSION["utente"] = $campi;
+                echo "login avvenuto con successo";
+              }else{
+                echo "<br>credenziali errate";
+              }
+
             }
           ?> 
-          <input type='submit'>
+          <br><input type='submit' value="login">
         </form>
     </div>
 
@@ -34,7 +54,7 @@
     
     <script>
       // set active
-      var active = document.getElementById('sidebar_map');
+      var active = document.getElementById('sidebar_login');
       active.classList.add('active');
     </script>
 
