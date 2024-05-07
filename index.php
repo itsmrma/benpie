cd34d0d4348e3af712d92bbb586600c385d7fe7e
Pietro <savioli.pietro@gmail.com>
2024-05-07 17:52:42 +0200

    tabella eventi prossimi

--- index.php
+++ index.php
@@ -3,46 +3,67 @@
 
     <head>
       <?php include 'head.html';?>
+      <link href="https://unpkg.com/material-components-web@latest/dist/material-components-web.min.css" rel="stylesheet">
+      <script src="https://unpkg.com/material-components-web@latest/dist/material-components-web.min.js"></script>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 
+      <style>
+        .mdc-data-table{
+          height: 50%;
+          width: 50%;
+        }
+      </style>
     </head>
 
     <body>
-      
+
       <?php include 'code.html';?>
 
       <div class="main-container">
-        
+
+      <div class="mdc-data-table">
+        <div class="mdc-data-table__table-container" >
+          <table class="mdc-data-table__table" aria-label="Dessert calories">
+            <thead>
+              <tr class="mdc-data-table__header-row">
+                <th class="mdc-data-table__header-cell" role="columnheader" scope="col">PROSSIMI EVENTI</th>
+                <th class="mdc-data-table__header-cell mdc-data-table__header-cell--numeric" role="columnheader" scope="col"><span class='material-symbols-outlined'>schedule</span>INIZIO</th>
+              </tr>
+            </thead>
+
+            <tbody class="mdc-data-table__content">
+              <?php 
+
+                $currentDate = date('Y-m-d');
+                $conn = new mysqli("localhost","root","","sagre");
+
+                if ($conn -> connect_error) {
+                  die("Errore di connessione ".$conn->connect_errno." ".$conn->connect_error);
+                }
+
+                $sql = "select id, denom, CAST(evento.data_inizio AS date) as data_inizio from evento where evento.data_inizio>='".$currentDate."'order by evento.data_inizio asc limit 10";
+
+                $prossimiEventi = $conn ->query($sql);
+
+                while($datiEventi = $prossimiEventi->fetch_assoc()){
+                  echo "
+                  <tr class='mdc-data-table__row'>
+                    <th class='mdc-data-table__cell' scope='row'><a href='infoEvento.php?idEvento=".$datiEventi['id']."' target='_blank' >".$datiEventi["denom"]."</a></th>
+                    <td class='mdc-data-table__cell mdc-data-table__cell--numeric'>".$datiEventi['data_inizio']."</td>
+                    
+                  </tr>";
+                }
+
+              ?>
 
 
-        <md-list style="max-width: 300px;">
+            </tbody>
+          </table>
+        </div>
+      </div>
+  
+      
 
-          <?php 
-
-            $currentDate = date('Y-m-d');
-            $conn = new mysqli("localhost","root","","sagre");
-
-            if ($conn -> connect_error) {
-              die("Errore di connessione ".$conn->connect_errno." ".$conn->connect_error);
-            }
-
-            $sql = "select id, denom from evento where evento.data_inizio>='".$currentDate."'order by evento.data_inizio asc limit 10";
-            
-            $prossimiEventi = $conn ->query($sql);
-
-            while($datiEventi = $prossimiEventi->fetch_assoc()){
-             
-                echo "
-                <md-list-item>
-                <a href='infoEvento.php?idEvento=".$datiEventi["id"]."' target='_blank'  ciao='employeeLink'>".$datiEventi["denom"]."</a>
-                <img slot='start' style='width: 56px' >
-                </md-list-item>
-                ";
-
-            }
-
-          ?>
-
-        </md-list>
 
 
 
