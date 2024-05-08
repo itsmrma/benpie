4cf3a233628adfcfe35fdd19285094c272477ef9
DilemmaticPerson <savioli.pietro@gmail.com>
2024-05-08 11:56:52 +0200

    query utenti loggati

--- index.php
+++ index.php
@@ -39,25 +39,31 @@
 
           <tbody class="mdc-data-table__content">
             <?php
-
+            session_start();
             $currentDate = date('Y-m-d');
             $conn = new mysqli("localhost", "root", "", "sagre");
 
             if ($conn->connect_error) {
               die("Errore di connessione " . $conn->connect_errno . " " . $conn->connect_error);
             }
-
+            
+            if(isset($_SESSION["email"])){
+              "select DISTINCT denom from preferiti inner join evento on evento.id = preferiti.idUtente where preferiti.idUtente =".$_SESSION['id']
+              $sql = "select denom from  as data_inizio from evento where evento.data_inizio>='" . $currentDate . "'order by evento.data_inizio asc limit 10";
+            }
             $sql = "select id, denom, CAST(evento.data_inizio AS date) as data_inizio from evento where evento.data_inizio>='" . $currentDate . "'order by evento.data_inizio asc limit 10";
 
             $prossimiEventi = $conn->query($sql);
 
             while ($datiEventi = $prossimiEventi->fetch_assoc()) {
+
               echo "
                   <tr class='mdc-data-table__row'>
                     <th class='mdc-data-table__cell' scope='row'><a href='infoEvento.php?idEvento=" . $datiEventi['id'] . "' target='_blank' ><md-text-button>" . $datiEventi["denom"] . "</md-text-button></a></th>
                     <td class='mdc-data-table__cell mdc-data-table__cell--numeric'>" . $datiEventi['data_inizio'] . "</td>
-                    
+                  
                   </tr>";
+
             }
 
             ?>
