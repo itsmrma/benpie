092f8fd54cde5f1e0c42a7b4c170040cb35cd8b5
itsmrma <71072396+itsmrma@users.noreply.github.com>
2024-05-17 11:35:28 +0200

    db

---
+++ 5biin-02.sql
@@ -0,0 +1,277 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4deb2+deb11u1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Creato il: Mag 17, 2024 alle 11:31
+-- Versione del server: 10.5.18-MariaDB-0+deb11u1
+-- Versione PHP: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `5biin-02`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `commento`
+--
+
+CREATE TABLE `commento` (
+  `id` int(11) NOT NULL,
+  `testo` mediumtext NOT NULL,
+  `dataOraPubblicazione` datetime NOT NULL,
+  `idUtente` int(11) NOT NULL,
+  `idCommentoPadre` int(11) NOT NULL,
+  `idEvento` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `comune`
+--
+
+CREATE TABLE `comune` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(50) DEFAULT NULL,
+  `cap` int(5) DEFAULT NULL,
+  `id_prov` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `evento`
+--
+
+CREATE TABLE `evento` (
+  `id` int(11) NOT NULL,
+  `denom` varchar(100) DEFAULT NULL,
+  `id_tipo` int(11) NOT NULL,
+  `n_ediz` varchar(50) DEFAULT NULL,
+  `descrizione` varchar(500) DEFAULT NULL,
+  `data_inizio` datetime DEFAULT NULL,
+  `ora_inizio` varchar(5) DEFAULT NULL,
+  `data_fine` datetime DEFAULT NULL,
+  `ora_fine` varchar(5) DEFAULT NULL,
+  `anno` year(4) DEFAULT NULL,
+  `istat` int(6) DEFAULT NULL,
+  `id_comune` int(11) NOT NULL,
+  `id_toponimo` int(11) NOT NULL,
+  `civico` varchar(10) DEFAULT NULL,
+  `nome_org` varchar(100) DEFAULT NULL,
+  `url` varchar(100) DEFAULT NULL,
+  `geo_x` varchar(30) DEFAULT NULL,
+  `geo_y` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `preferiti`
+--
+
+CREATE TABLE `preferiti` (
+  `idEvento` int(11) NOT NULL,
+  `idUtente` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `provincia`
+--
+
+CREATE TABLE `provincia` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `tipo`
+--
+
+CREATE TABLE `tipo` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `toponimo`
+--
+
+CREATE TABLE `toponimo` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `utenti`
+--
+
+CREATE TABLE `utenti` (
+  `nomeUtente` varchar(30) NOT NULL,
+  `email` varchar(60) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `idUtente` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `commento`
+--
+ALTER TABLE `commento`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idUtente` (`idUtente`),
+  ADD KEY `idEvento` (`idEvento`);
+
+--
+-- Indici per le tabelle `comune`
+--
+ALTER TABLE `comune`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_prov` (`id_prov`);
+
+--
+-- Indici per le tabelle `evento`
+--
+ALTER TABLE `evento`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_comune` (`id_comune`),
+  ADD KEY `id_tipo` (`id_tipo`),
+  ADD KEY `id_toponimo` (`id_toponimo`);
+ALTER TABLE `evento` ADD FULLTEXT KEY `descrizione` (`descrizione`);
+
+--
+-- Indici per le tabelle `preferiti`
+--
+ALTER TABLE `preferiti`
+  ADD PRIMARY KEY (`idEvento`,`idUtente`),
+  ADD KEY `idUtente` (`idUtente`);
+
+--
+-- Indici per le tabelle `provincia`
+--
+ALTER TABLE `provincia`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `tipo`
+--
+ALTER TABLE `tipo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `toponimo`
+--
+ALTER TABLE `toponimo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `utenti`
+--
+ALTER TABLE `utenti`
+  ADD PRIMARY KEY (`idUtente`);
+
+--
+-- AUTO_INCREMENT per le tabelle scaricate
+--
+
+--
+-- AUTO_INCREMENT per la tabella `commento`
+--
+ALTER TABLE `commento`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `comune`
+--
+ALTER TABLE `comune`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `evento`
+--
+ALTER TABLE `evento`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `provincia`
+--
+ALTER TABLE `provincia`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `tipo`
+--
+ALTER TABLE `tipo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `toponimo`
+--
+ALTER TABLE `toponimo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `utenti`
+--
+ALTER TABLE `utenti`
+  MODIFY `idUtente` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Limiti per le tabelle scaricate
+--
+
+--
+-- Limiti per la tabella `commento`
+--
+ALTER TABLE `commento`
+  ADD CONSTRAINT `commento_ibfk_1` FOREIGN KEY (`idUtente`) REFERENCES `utenti` (`idUtente`),
+  ADD CONSTRAINT `commento_ibfk_2` FOREIGN KEY (`idEvento`) REFERENCES `evento` (`id`);
+
+--
+-- Limiti per la tabella `comune`
+--
+ALTER TABLE `comune`
+  ADD CONSTRAINT `comune_ibfk_1` FOREIGN KEY (`id_prov`) REFERENCES `provincia` (`id`);
+
+--
+-- Limiti per la tabella `evento`
+--
+ALTER TABLE `evento`
+  ADD CONSTRAINT `evento_ibfk_1` FOREIGN KEY (`id_comune`) REFERENCES `comune` (`id`),
+  ADD CONSTRAINT `evento_ibfk_2` FOREIGN KEY (`id_tipo`) REFERENCES `tipo` (`id`),
+  ADD CONSTRAINT `evento_ibfk_3` FOREIGN KEY (`id_toponimo`) REFERENCES `toponimo` (`id`);
+
+--
+-- Limiti per la tabella `preferiti`
+--
+ALTER TABLE `preferiti`
+  ADD CONSTRAINT `preferiti_ibfk_1` FOREIGN KEY (`idEvento`) REFERENCES `evento` (`id`),
+  ADD CONSTRAINT `preferiti_ibfk_2` FOREIGN KEY (`idUtente`) REFERENCES `utenti` (`idUtente`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
